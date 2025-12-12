4f2fe3068335a60e4778191c2f31ceacc032fd84
RohanDaCoder <rohanbmgo2010@gmail.com>
2025-12-12 17:57:05 +0600

    feat: add github link

--- src/main/kotlin/me/rohandacoder/untitledGeneratorMaker/commands/UgmCommand.kt
+++ src/main/kotlin/me/rohandacoder/untitledGeneratorMaker/commands/UgmCommand.kt
@@ -14,8 +14,9 @@ class UgmCommand(private val plugin: UntitledGeneratorMaker) {
     @Suppress("UNUSED")
     fun execute(@Sender sender: CommandSender) {
         sender.sendRichMessage("<dark_gray>==================================================")
-        sender.sendRichMessage("<yellow> Untitled Generator Maker <gray>${plugin.pluginMeta.version}")
+        sender.sendRichMessage("<yellow><bold> Untitled Generator Maker <gray>${plugin.pluginMeta.version}")
         sender.sendRichMessage("<gray>   By <blue>${plugin.pluginMeta.authors} ")
+        sender.sendRichMessage("<white>  <hover:show_text:\"<blue>(CLICK ME)\"><click:open_url:'https://github.com/RohanDaCoder/UntitledGeneratorMaker'>[GITHUB]</click> ")
         sender.sendRichMessage("<green> a gen server maker ")
         sender.sendRichMessage("<dark_gray>==================================================")
     }
