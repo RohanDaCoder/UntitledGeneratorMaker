abeed34ff5322310f76dbfcd9a9c5847d91f8493
RohanDaCoder <rohanbmgo2010@gmail.com>
2025-12-12 17:48:18 +0600

    feat: ugm command (reload, info)

---
+++ src/main/kotlin/me/rohandacoder/untitledGeneratorMaker/commands/UgmCommand.kt
@@ -0,0 +1,33 @@
+package me.rohandacoder.untitledGeneratorMaker.commands
+
+import dev.rollczi.litecommands.annotations.command.Command
+import dev.rollczi.litecommands.annotations.context.Sender
+import dev.rollczi.litecommands.annotations.execute.Execute
+import dev.rollczi.litecommands.annotations.permission.Permission
+import me.rohandacoder.untitledGeneratorMaker.UntitledGeneratorMaker
+import org.bukkit.command.CommandSender
+
+@Command(name = "ugm")
+@Suppress("UNUSED")
+class UgmCommand(private val plugin: UntitledGeneratorMaker) {
+    @Execute()
+    @Suppress("UNUSED")
+    fun execute(@Sender sender: CommandSender) {
+        sender.sendRichMessage("<dark_gray>==================================================")
+        sender.sendRichMessage("<yellow> Untitled Generator Maker <gray>${plugin.pluginMeta.version}")
+        sender.sendRichMessage("<gray>   By <blue>${plugin.pluginMeta.authors} ")
+        sender.sendRichMessage("<green> a gen server maker ")
+        sender.sendRichMessage("<dark_gray>==================================================")
+    }
+
+    @Execute(name = "reload")
+    @Permission("untitledgeneratormaker.reload")
+    @Suppress("UNUSED")
+    fun executeReload(@Sender sender: CommandSender) {
+        plugin.reloadConfig()
+        sender.sendRichMessage(
+            plugin.config.getString("messages.reload") ?: "<green>Configuration reloaded!"
+        )
+        plugin.logger.info("Config reloaded!")
+    }
+}
