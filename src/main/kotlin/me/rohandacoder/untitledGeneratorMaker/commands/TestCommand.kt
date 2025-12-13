f38f802dfd0df7fa403d9ff9221eb9fef9604423
RohanDaCoder <rohanbmgo2010@gmail.com>
2025-12-13 15:36:39 +0600

    feat: test command for testing purposes

--- src/main/kotlin/me/rohandacoder/untitledGeneratorMaker/commands/TestCommand.kt
+++ src/main/kotlin/me/rohandacoder/untitledGeneratorMaker/commands/TestCommand.kt
@@ -4,12 +4,11 @@ import dev.rollczi.litecommands.annotations.command.Command
 import dev.rollczi.litecommands.annotations.context.Sender
 import dev.rollczi.litecommands.annotations.execute.Execute
 import dev.rollczi.litecommands.annotations.permission.Permission
-import me.rohandacoder.untitledGeneratorMaker.UntitledGeneratorMaker
 import org.bukkit.command.CommandSender
 
 @Command(name = "test")
 @Permission("untitledgeneratormaker.test")
-class TestCommand(private val plugin: UntitledGeneratorMaker) {
+class TestCommand() {
 
     @Execute
     @Suppress("UNUSED")
